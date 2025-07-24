ad94c87dd21221d93a57d4a5bbcdcbe57e42f1be
EmacsIsBetterThanVI <rdwisema@gmail.com>
2025-07-24 11:13:30 -0400

    Updated graphics, to be graphical.

--- FishClicker.py
+++ FishClicker.py
@@ -1,4 +1,37 @@
 from PyWigit import *
+class ImageTextButton():
+  def __init__(self, text, image1, image2=None, LeftClick=None, RightClick=None, Mouse=None):
+    self.text = text
+    self.image1 = image1
+    self.image2 = image2
+    self.image = False
+    self.LeftClick=LeftClick
+    self.RightClick=RightClick
+    self.Mouse=Mouse
+    self.rect = self.image1.get_rect()
+  def draw(self, screen, loc):
+    self.rect = screen.blit(self.image2 if self.image else self.image1, loc)
+    screen.blit(self.text, (loc[0]+self.image1.get_width(), loc[1]))
+  def Click(self, event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if self.rect.collidepoint(event.pos):
+        if event.button == 1 and self.LeftClick:
+          self.LeftClick(self, False)
+        elif event.button == 2 and self.RightClick:
+          self.RightClick(self, False)
+        elif event.button == 3 and self.Mouse:
+          self.Mouse(self, False)
+        return True
+    elif event.type == pygame.MOUSEBUTTONUP:
+      if self.rect.collidepoint(event.pos):
+        if event.button == 1 and self.LeftClick:
+          self.LeftClick(self, True)
+        elif event.button == 2 and self.RightClick:
+          self.RightClick(self, True)
+        elif event.button == 3 and self.Mouse:
+          self.Mouse(self, True)
+        return True
+    return False
 import json
 import os
 screen = NewScreen((800, 600))
@@ -10,8 +43,8 @@ TotalCash = 0
 Buttons = []
 Achivements = []
 PossibleAchivements = {
-    "Starting Out":(1, 1), # based on button 1, click button 1, 1 time
-    "Perstitance":(1, 1000),
+    "Starting Out":(2, 1), # based on button 1, click button 1, 1 time
+    "Perstitance":(2, 1000),
     "Millionaire":(0, 10**6),
     "Billionaire":(0, 10**9),
     "Surpasing Infinity":(0, 10**16) # based on cash, requires hiting infinity
@@ -32,9 +65,13 @@ def checkAchivements():
             MessageQueue.append("************* David *************")
             MessageQueue.append("************ Raphael ************")
             MessageQueue.append("Now, go enjoy the rest of your life")
-      elif PossibleAchivements[j][0] >= 1:
+      elif PossibleAchivements[j][0] == 0:
+        if PossibleAchivements[j][1] <= TotalCash:
+          Achivements.append(j)
+          MessageQueue.append(j)
+      elif PossibleAchivements[j][0] >= 2:
         try:
-          if PossibleAchivements[j][1] <= Buttons[PossibleAchivements[j][0]-1][3]:
+          if PossibleAchivements[j][1] <= Buttons[PossibleAchivements[j][0]-2][3]:
             Achivements.append(j)
             MessageQueue.append(j)
         except Exception as e:
@@ -87,32 +124,39 @@ def SaveGame():
         data.append([j[1], j[2], j[3]])
     f.write(json.dumps({"version":2, "buttons":data, "cash":cash, "achivements": Achivements, "TotalCash": TotalCash}))
     f.close()
-def Button0Click():
-    global cash
-    if Buttons[0][1] <= cash:
-        cash -= Buttons[0][1]
-        cash += Buttons[0][2]
-        Buttons[0][3] += 1
+def Button0Click(self, UP: bool):
+    global cash, TotalCash
+    if not UP:
+      self.image = True
+      cash += Buttons[0][2]
+      TotalCash += Buttons[0][2]
+      Buttons[0][3] += 1
+    else:
+      self.image = False
 i = 1
 MessageTicks = 0
 def NewUpgrade():
     global i
-    exec(f"""def Button{i}Click():
+    exec(f"""def Button{i}Click(self, UP):
     global cash
-    if Buttons[{i}][1] <= cash:
+    if not UP:
+      if Buttons[{i}][1] <= cash:
         cash -= Buttons[{i}][1]
         Buttons[{i-1}][2] += Buttons[{i}][2]
         Buttons[{i}][1] = int(Buttons[{i}][1] * 1.1)
         if Buttons[{i}][3] == 0:
             NewUpgrade()
         Buttons[{i}][3] += 1
-Buttons.append([Button(font.render("Button {i+1}($.): .", True, White), LeftClick=Button{i}Click), 10**(i+1), 1, 0])
+        self.image = True
+    else:
+      self.image = False
+Buttons.append([ImageTextButton(font.render("Button {i+1}($.): .", True, White), pygame.image.load(os.path.dirname(__file__)+"/button{i+1}.png"), pygame.image.load(os.path.dirname(__file__)+"/button{i+1}P.png"), LeftClick=Button{i}Click), 10**(i+1), 1, 0])
 """)
     i+=1
 Save = Button(font.render("Save", True, White), LeftClick=SaveGame)
 AchivmentsButton = Button(font.render("Achivments", True, Green), LeftClick=lambda : Window.ChangeScrn(2))
 UpgradesButton = Button(font.render("Upgrades", True, Green), LeftClick=lambda : Window.ChangeScrn(1))
-Buttons.append([Button(font.render("Button: .", True, White), LeftClick=Button0Click), 0, 1, 0])
+Buttons.append([ImageTextButton(font.render("Button: .", True, White), pygame.image.load(os.path.dirname(__file__)+"/button1.png"), pygame.image.load(os.path.dirname(__file__)+"/button1P.png"), LeftClick=Button0Click), 0, 1, 0])
 if os.path.exists("ButtonClicker.save"):
     f = open("ButtonClicker.save")
     data = json.loads(f.read())
@@ -120,8 +164,8 @@ if os.path.exists("ButtonClicker.save"):
 else:
     NewUpgrade()
     MessageQueue.append("Welcome to Button Clicker!")
-    MessageQueue.append("Anything that says Button...")
-    MessageQueue.append("is a button for you to click.")
+    #MessageQueue.append("Anything that says Button...")
+    #MessageQueue.append("is a button for you to click.")
     MessageQueue.append("We hope you enjoy the \"game\"")
 def DrawGame(screen):
     checkAchivements()
@@ -139,13 +183,13 @@ def DrawGame(screen):
         screen.blit(font.render(f"CASH: {cash:,}", True, White), (20, 0))
     Save.draw(screen, (300, 0))
     AchivmentsButton.draw(screen, (400, 0))
-    Buttons[0][0].display = font.render(f"Button: {Buttons[0][3]:,}, Increase Cash by {Buttons[0][2]:,} per click", True, White)
+    Buttons[0][0].text = font.render(f": {Buttons[0][3]:,}, Increase Cash by {Buttons[0][2]:,} per click", True, White)
     Buttons[0][0].draw(screen, (20, 24))
-    Buttons[1][0].display = font.render(f"Button 2(${Buttons[1][1]:,}): {Buttons[1][3]:,}, Increase Button value by {Buttons[1][2]:,} per click", True, White)
-    Buttons[1][0].draw(screen, (20, 48))
+    Buttons[1][0].text = font.render(f"(${Buttons[1][1]:,}): {Buttons[1][3]:,}, Increase Button value by {Buttons[1][2]:,} per click", True, White)
+    Buttons[1][0].draw(screen, (20, 49))
     for j in range(2, len(Buttons)):
-        Buttons[j][0].display = font.render(f"Button {j+1}(${Buttons[j][1]:,}): {Buttons[j][3]:,}, Increase Button {j} value by {Buttons[j][2]:,} per click", True, White)
-        Buttons[j][0].draw(screen, (20, 24+24*j))
+        Buttons[j][0].text = font.render(f"(${Buttons[j][1]:,}): {Buttons[j][3]:,}, Increase Button {j} value by {Buttons[j][2]:,} per click", True, White)
+        Buttons[j][0].draw(screen, (20, 24+25*j))
 def InputGame(event):
     if Save.Click(event):
         return True
